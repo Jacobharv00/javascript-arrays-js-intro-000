a4a026f76130d20cd7ed032f6396ac786e7e8d42
Jacob Harvey <jacobharv00@gmail.com>
2021-01-26 17:21:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,6 +42,6 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   let arrayOk = [1, 2, 3, 4]
-  //arrayOk.shift()
-  return arrayOk.shift()
+  arrayOk.shift()
+  return arrayOk
 }
