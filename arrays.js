4962ec2075c31cc32ab471b0459eab0efd19d86f
Jacob Harvey <jacobharv00@gmail.com>
2021-01-26 17:33:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,14 +41,14 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  let poop = ['element', 'element', 'element']
+  let poop = [array]
   poop.shift()
   return poop
 }
 
 
 function removeElementFromBeginningOfArray(array) {
-  let taco = ['element', 'element', 'element']
+  let taco = [array]
   taco.slice(1)
   return taco
 }
